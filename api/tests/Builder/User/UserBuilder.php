c05c02fec44140527b1eab9bff86d4ee252602e4
Nikitos <gladkih@index-art.ru>
2020-02-18 22:19:16 +0500

    Added Model - User

---
+++ api/tests/Builder/User/UserBuilder.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Builder\User;
+
+use App\Model\User\Entity\User\Email;
+use App\Model\User\Entity\User\User;
+use App\Model\User\Entity\User\Id;
+
+class UserBuilder
+{
+    private Id $id;
+    private \DateTimeImmutable $date;
+
+    private Email $email;
+    private string $hash;
+    private string $token;
+    private bool $confirmed = false;
+
+    private string $network;
+    private string $identity;
+
+    public function __construct()
+    {
+        $this->id = Id::next();
+        $this->date = new \DateTimeImmutable();
+    }
+
+    public function viaEmail(Email $email = null, string $hash = null, string $token = null): self
+    {
+        $clone = clone $this;
+        $clone->email = $email ?? new Email('mail@app.test');
+        $clone->hash = $hash ?? 'hash';
+        $clone->token = $token ?? 'token';
+
+        return $clone;
+    }
+
+    public function confirmed(): self
+    {
+        $clone = clone $this;
+        $clone->confirmed = true;
+
+        return $clone;
+    }
+
+    public function viaNetwork(string $network = null, string $identity = null): self
+    {
+        $clone = clone $this;
+        $clone->network = $network ?? 'facebook';
+        $clone->identity = $identity ?? 'facebook_identity_001';
+
+        return $clone;
+    }
+
+    public function build(): User
+    {
+        if (!empty($this->email)) {
+            $user = User::signUpByEmail(
+                $this->id,
+                $this->date,
+                $this->email,
+                $this->hash,
+                $this->token
+            );
+
+            if ($this->confirmed) {
+                $user->confirmSignUp();
+            }
+
+            return $user;
+        }
+
+        if (!empty($this->network)) {
+            return User::signUpByNetwork(
+                $this->id,
+                $this->date,
+                $this->network,
+                $this->identity
+            );
+        }
+
+        throw new \BadMethodCallException('Specify via method.');
+    }
+}
